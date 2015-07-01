c02402caf97e66bfe32a9f17f8239027e3d87d44
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-07-01 15:52:56 +0800

    pos_v0 finished

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,4 +1,14 @@
 //TODO: Please write code in this file.
 function printReceipt(inputs) {
-
+  var str='***<没钱赚商店>收据***\n';
+  var c_al1=0;
+    for(var i=0;i<inputs.length;i++){
+      c_al1+=inputs[i].count*inputs[i].price;
+      str+='名称：'+inputs[i].name+'，数量：'+inputs[i].count+inputs[i].unit+'，单价：'+inputs[i].price.toFixed(2)+'(元)，小计：'+(inputs[i].count*inputs[i].price).toFixed(2)+'(元)\n';
+    }
+   str+='----------------------\n';
+   str+='总计：'+c_al1.toFixed(2)+'(元)\n';
+   str+='**********************';
+   console.log(str);
 }
+
